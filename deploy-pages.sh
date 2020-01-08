ca95109f0fee15032935dea1a945f59dd4d2870f
light <light0x00@163.com>
2020-01-08 23:09:46 +0800

    dep script

--- deploy-pages.sh
+++ deploy-pages.sh
@@ -1,6 +1,8 @@
-rm -rf _pages
-cp -r packages/web/dist _pages
-cd _pages
+worktree='gh-pages'
+
+rm -rf "$worktree/*"
+cp -r packages/web/dist "$worktree/"
+cd $worktree
 git add --all
 git commit -m "deploy: git-pages"
-git push
+git push -u origin gh-pages
